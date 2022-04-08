a2a1cece974a1f0c9ed3afe22ce70c1a50e19c26
Rusyaidi <rusyaidiramly@gmail.com>
2022-04-08 13:35:14 +0800

    Add random alphanumeric generator service

---
+++ Services/RndAlphaService.cs
@@ -0,0 +1,16 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RestAPI.Services
+{
+    public static class RndAlphaService
+    {
+
+        public static string String(int length)
+        {
+            return "adasdasd";
+        }
+
+    }
+}
