a97df5f956a3fad0b6e953449be1cdd4d01425e7
Rusyaidi <rusyaidiramly@gmail.com>
2022-04-08 13:39:44 +0800

    Add random alphanumeric function

--- Services/RndAlphaService.cs
+++ Services/RndAlphaService.cs
@@ -1,15 +1,28 @@
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.IO;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace RestAPI.Services
 {
     public static class RndAlphaService
     {
 
-        public static string String(int length)
+        public static string Generate(int length)
         {
-            return "adasdasd";
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = System.BitConverter.ToUInt32(uintBuffer, 0);
+                    res.Append(valid[(int)(num % (uint)valid.Length)]);
+                }
+            }
+
+            return res.ToString();
         }
 
     }
